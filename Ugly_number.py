769010de8f17aaf2ec95010bf3ee4fc482ff8a2c
Seethanveshi <84505572+Seethanveshi@users.noreply.github.com>
2022-10-09 16:32:21 +0530

    def isUgly(n):
        if (n == 1):
            return 1
        if (n <= 0):
            return 0
    
        if (n % 2 == 0):
            return (isUgly(n // 2))
        if (n % 3 == 0):
            return (isUgly(n // 3))
        if (n % 5 == 0):
            return (isUgly(n // 5))
    
        return 0
    n=int(input())
    no = isUgly(n)
    if (no == 1):
        print("Ugly Number")
    else:
        print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,21 @@
+def isUgly(n):
+
+    if (n == 1):
+        return 1
+    if (n <= 0):
+        return 0
+
+    if (n % 2 == 0):
+        return (isUgly(n // 2))
+    if (n % 3 == 0):
+        return (isUgly(n // 3))
+    if (n % 5 == 0):
+        return (isUgly(n // 5))
+
+    return 0
+n=int(input())
+no = isUgly(n)
+if (no == 1):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
