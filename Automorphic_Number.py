9a9ae8e604f11c55aeda97a3eec8a08d82128ed4
Seethanveshi <84505572+Seethanveshi@users.noreply.github.com>
2022-10-09 15:07:20 +0530

    n=int(input()) c=0 t=n*n for i in str(n):     c+=1 s=t%(10**c) if n==s:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,10 +1,10 @@
 n=int(input())
-t=n*n
 c=0
+t=n*n
 for i in str(n):
     c+=1
-f=t%(10**c)
-if f==n:
+s=t%(10**c)
+if n==s:
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
