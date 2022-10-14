25b8bb895773fe19a8cea7569ebe9b002230f7b7
Seethanveshi <84505572+Seethanveshi@users.noreply.github.com>
2022-10-14 11:33:41 +0530

    tc=int(input()) for tc in range(tc):     n=int(input())     f=n     t=n     while True:         c=0         t1=0         for i in range(2,int(n**0.5)+1,1):             if n%i==0:                 c+=1         if c==0:             t1=n             break         else:             n+=1     while True:         c1=0         t2=0         for j in range(t,0,-1):             if t%j==0:                 c1+=1         if c1==2:             t2=t             break         else:             t-=1     if t1-f>=f-t2:         print(t2)     elif t1-f<f-t2:         print(t1)

---
+++ Nearest_Prime.py
@@ -0,0 +1,31 @@
+tc=int(input())
+for tc in range(tc):
+    n=int(input())
+    f=n
+    t=n
+    while True:
+        c=0
+        t1=0
+        for i in range(2,int(n**0.5)+1,1):
+            if n%i==0:
+                c+=1
+        if c==0:
+            t1=n
+            break
+        else:
+            n+=1
+    while True:
+        c1=0
+        t2=0
+        for j in range(t,0,-1):
+            if t%j==0:
+                c1+=1
+        if c1==2:
+            t2=t
+            break
+        else:
+            t-=1
+    if t1-f>=f-t2:
+        print(t2)
+    elif t1-f<f-t2:
+        print(t1)
