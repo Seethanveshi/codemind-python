b3166b1b562a692645f4b63c2be20729f6597cd7
Seethanveshi <84505572+Seethanveshi@users.noreply.github.com>
2022-10-30 07:58:45 +0530

    def prime(n):     p="True"     for i in range(2,int(n**0.5)+1,1):         if n%i==0:             p="False"     if p=="True":         p="True"     else:         p="False"     return p n=int(input()) p=prime(n) if p=="True":     n1=n     rev=0     while n1>0:         r=n1%10         rev=rev*10+r         n1=n1//10     if p==prime(rev):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,29 +1,25 @@
+def prime(n):
+    p="True"
+    for i in range(2,int(n**0.5)+1,1):
+        if n%i==0:
+            p="False"
+    if p=="True":
+        p="True"
+    else:
+        p="False"
+    return p
 n=int(input())
-c=0
-for i in range(1,n+1,1):
-    if n%i==0:
-        c+=1
-if c==2:
-    y=5
-else:
-    y=10
-sum=0
-x=n
-while n!=0:
-    r=n%10
-    sum=sum*10+r
-    n=n//10
-c1=0
-for j in range(1,sum+1,1):
-    if sum%j==0:
-        c1+=1
-if c1==2:
-    z=5
-else:
-    z=10
-if y==5 and z==5:
-    print("circular prime")
-elif y==5 or z==5:
-    print("prime but not a circular prime")
+p=prime(n)
+if p=="True":
+    n1=n
+    rev=0
+    while n1>0:
+        r=n1%10
+        rev=rev*10+r
+        n1=n1//10
+    if p==prime(rev):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
     print("not prime")
